aeca20db33d3a1f4d7115ec9223b943482989937
AishCold <72552823+AishCold@users.noreply.github.com>
2021-02-01 04:48:24 +0000

    Add files via upload

---
+++ N Green Bottles Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ten green bottles");
+
+        int count = 10;
+
+        do
+        {
+            Console.WriteLine(count + " green bottles standing on a wall.");
+            Console.WriteLine(count + " green bottles standing on a wall.");
+            Console.WriteLine("but if 1 green bottle should accidently fall");   
+            Console.WriteLine("there'd be " + (count - 1)  + " green bottles standing on a wall.");
+            count = count - 1;
+
+        } while (count >= 1);
+    }
+}
+       
---
+++ Ten Guesses Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Higher Lower Game");
+
+        Console.WriteLine("Guess the random number!");
+        string inputString = Console.ReadLine();
+        int userGuess = int.Parse(inputString);
+
+        Random random = new Random();
+
+        int secretNumber = random.Next(1, 101);
+        
+
+        while (userGuess != secretNumber)
+        {
+            if (userGuess > secretNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (userGuess < secretNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+
+        Console.WriteLine("Congratulations, you win!");
+    }
+}
